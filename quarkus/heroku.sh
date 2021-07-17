3e2c9f9c2996ea813df1fc0bbd7fc680357ac372
Felipe Henrique Gross Windmoller <felipewind@gmail.com>
2021-07-17 15:55:01 -0300

    update

--- quarkus/heroku.sh
+++ quarkus/heroku.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # =============================================================================
-# This script automatically splits the env variable DATASOURCE_URL from
-# Heroku into the three JDBC variables needed from Quarkus.
+# This script automatically splits the Heroku ENV DATASOURCE_URL variable
+# into the three JDBC variables needed from Quarkus.
 # 
-# It will only do the split if the DB_AUTO_SPLIT is set to "true".
+# It will only do the split if the DB_HEROKU_SPLIT is set to "true".
 #
-# If you set DB_AUTO_SPLIT to 'false', you must pass the Quarkus parameters:
-#   - DATASOURCE_JDBC_URL;
-#   - DATASOURCE_JDBC_USER;
-#   - DATASOURCE_JDBC_PASSWORD.
+# If you set DB_HEROKU_SPLIT to 'false', you must pass the Quarkus parameters:
+#   - DB_JDBC_URL;
+#   - DB_JDBC_USER;
+#   - DB_JDBC_PASSWORD.
 # 
-# For test purposes, you can set the ECHO_DB_VALUES to 'true' and check if the
+# For test purposes, you can set the DB_ECHO_VALUES to 'true' and check if the
 # values are correct.
 # 
 # Pattern of DATASOURCE_URL from Heroku:
@@ -25,25 +25,28 @@
 # 
 # =============================================================================
 
-echo DB_AUTO_SPLIT=[$DB_AUTO_SPLIT] 
+echo DB_HEROKU_SPLIT=[$DB_HEROKU_SPLIT] 
 
 # check for 'true' in string (case insensitive)
-if [[ "${DB_AUTO_SPLIT,,}" == "true" ]]; then
+if [[ "${DB_HEROKU_SPLIT,,}" == "true" ]]; then
 
-  export DATASOURCE_JDBC_URL=jdbc:postgresql://${DATABASE_URL#*@}
+  # cut the DATABASE_URL after '@'
+  export DB_JDBC_URL=jdbc:postgresql://${DATABASE_URL#*@}
 
-  export DATASOURCE_JDBC_USER=$(expr $DATABASE_URL : '.*/\([^:]*\):.*')
+  # substring the DATABASE_URL between '//' and ':'
+  export DB_JDBC_USER=$(expr $DATABASE_URL : '.*/\([^:]*\):.*')
 
-  export DATASOURCE_JDBC_PASSWORD=$(expr $DATABASE_URL : '.*:\([^@]*\)@.*')
+  # substring the DATABASE_URL between ':' and '@'
+  export DB_JDBC_PASSWORD=$(expr $DATABASE_URL : '.*:\([^@]*\)@.*')
 
 fi
 
 # check for 'true' in string (case insensitive)
-if [[ "${ECHO_DB_VALUES,,}" == "true" ]]; then
+if [[ "${DB_ECHO_VALUES,,}" == "true" ]]; then
 
   echo DATABASE_URL=[$DATABASE_URL]
-  echo DATASOURCE_JDBC_URL=[$DATASOURCE_JDBC_URL]
-  echo DATASOURCE_JDBC_USER=[$DATASOURCE_JDBC_USER] 
-  echo DATASOURCE_JDBC_PASSWORD=[$DATASOURCE_JDBC_PASSWORD]
+  echo DB_JDBC_URL=[$DB_JDBC_URL]
+  echo DB_JDBC_USER=[$DB_JDBC_USER] 
+  echo DB_JDBC_PASSWORD=[$DB_JDBC_PASSWORD]
 
 fi
